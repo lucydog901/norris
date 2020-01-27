8f1a72c460006d4872c2eb77cdad9882b0fd7a8f
Thomasin Durgin <td@thomasin.com>
2020-01-27 18:40:42 +0000

    added tests

--- spec/features/index_feature_spec.rb
+++ spec/features/index_feature_spec.rb
@@ -9,14 +9,24 @@ RSpec.describe "Search", type: :feature do
     end
   end
 
+
 describe "facts#index action" do
-    it "should successfully display fact" do
+    it "should successfully display results" do
       visit root_path
-     expect(page).to have_content("keyword")
+     page.fill_in 'keyword', with: 'Cat'
+     click_button('Search')
+     expect(page).to have_selector("#joke") # checks for id 'joke'
     end
   end
 
-
+  describe "facts#index action" do
+    it "should successfully display error" do
+      visit root_path
+     page.fill_in 'keyword', with: 'Alabama'
+     click_button('Search')
+     expect(page).to have_selector(".error_msgs") # checks for class 'error_msgs'
+    end
+  end
 
    
 
