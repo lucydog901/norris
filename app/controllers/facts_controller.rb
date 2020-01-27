0a489c86116b0dc318347c32d1a9ca2983ba78cd
Thomasin Durgin <td@thomasin.com>
2020-01-27 17:43:03 +0000

    fixed spacing

--- app/controllers/facts_controller.rb
+++ app/controllers/facts_controller.rb
@@ -31,8 +31,6 @@ class FactsController < ApplicationController
 
   def find_keyword(keyword)
     response = HTTParty.get("https://api.chucknorris.io/jokes/search?query=#{keyword}")
-
-
     result = JSON.parse(response.body)["result"]
 
     unless result.nil? or result.length == 0
@@ -44,8 +42,6 @@ class FactsController < ApplicationController
 
   def find_random
     response = HTTParty.get("https://api.chucknorris.io/jokes/random")
-
-
     result = JSON.parse(response.body)
 
     return result
@@ -55,8 +51,6 @@ class FactsController < ApplicationController
     else
       return nil
     end
-
-
   end
 end
 
